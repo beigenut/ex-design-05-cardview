a147060a747905f4760c005db782dfe75979c8ff
beigenut <beigenut@gmail.com>
2019-05-21 22:37:19 -0400

    Add a portfolio design

---
+++ fullPage.js
@@ -0,0 +1,39 @@
+// variables
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+// toggle menu 
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['#B8AE9C', 'transparent', 'transparent', '#5C832F', '#B8B89F'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .3)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+}); 
+
+
+// SECTION 3 SCRIPT
+$(document).on('mouseover', '.section-3-column', function(){
+  $(this).addClass('active').siblings().removeClass('active')
+})
