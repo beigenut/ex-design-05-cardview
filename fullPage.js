f00660f6859e6b770ce2d3bb46fb3474381eaf69
Beigenut <beigenut@gmail.com>
2019-08-14 13:37:49 -0400

    Add accodian menu

--- fullPage.js
+++ fullPage.js
@@ -3,39 +3,39 @@ var $header_top = $('.header-top');
 var $nav = $('nav');
 
 // toggle menu 
-$header_top.find('a').on('click', function() {
+$header_top.find('a').on('click', function () {
   $nav.toggleClass('open-list');
   $(this).parent().toggleClass('x-toggle')
 });
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#B8AE9C', '#1B1B1C', 'transparent', '#40030E', '#40030E', '#FFAC00', '#2F3B40', '#36363c', 'transparent'],
+  sectionsColor: ['#B8AE9C', '#1B1B1C', 'transparent', '#40030E', '#40030E', '#FFAC00', '#2F3B40', '#36363c', 'transparent', 'transparent'],
   sectionSelector: '.vertical-scrolling',
   navigation: true,
   slidesNavigation: true,
   controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection', "section9"],
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection', "section9", "section10"],
   menu: '#menu',
 
-  afterLoad: function(anchorLink, index) {
+  afterLoad: function (anchorLink, index) {
     $header_top.css('background', 'rgba(0, 47, 77, .3)');
     $nav.css('background', 'rgba(0, 47, 77, .3)');
     if (index == 6) {
-        $('#fp-nav').hide();
-      }
+      $('#fp-nav').hide();
+    }
   },
 
-  onLeave: function(index, nextIndex, direction) {
-    if(index == 6) {
+  onLeave: function (index, nextIndex, direction) {
+    if (index == 6) {
       $('#fp-nav').show();
     }
   },
-}); 
+});
 
 
 // SECTION 3 SCRIPT
-$(document).on('mouseover', '.section-3-column', function(){
+$(document).on('mouseover', '.section-3-column', function () {
   $(this).addClass('active').siblings().removeClass('active')
 })
 
@@ -67,10 +67,10 @@ const $searchBox = $('.search-box-js');
 const $searchInput = $('.input-js');
 const $searchBtn = $('.search-btn');
 
-$searchInput.focus(function() {
+$searchInput.focus(function () {
   $(this).parent().toggleClass('focused')
 })
 
-$searchInput.focusout(function() {
+$searchInput.focusout(function () {
   $(this).parent().toggleClass('focused')
 })
